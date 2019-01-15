78e45676e2ca2a474b49ac01a71c02ac1ff5f17f
dima kiselev <d.s.matroskin@gmail.com>
2019-01-15 23:49:11 +0300

    feat: add task-1 touch slider

---
+++ task-1/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrow: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        easing: true,
+        cssEase: 'linear'
+    });
+});
